c5c4881dcdb019c17d9de8e79e7f50d2a477ab3f
t_woong <113299829+taewoong21@users.noreply.github.com>
2025-08-18 09:58:54 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    reverse(my_string.begin(), my_string.end());
+    
+    return my_string;
+}
