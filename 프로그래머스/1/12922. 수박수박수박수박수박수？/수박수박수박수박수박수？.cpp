3d80cee77459413dd80d542abbcf7324924a5557
t_woong <113299829+taewoong21@users.noreply.github.com>
2025-08-22 09:17:05 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    for(int i=1; i<=n; i++)
+    {
+        answer += (i%2==0) ? "박" : "수";    
+    }
+    
+    return answer;
+}
