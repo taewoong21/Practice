b616894a62575281357e31134781a628c3aec6ea
t_woong <113299829+taewoong21@users.noreply.github.com>
2025-09-04 09:08:21 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    int remainder = 0;
+    
+    while(n >= a)
+    {
+        answer += (n/a)*b;
+        remainder = n%a;
+        n = ((n/a)*b) + remainder; 
+    }
+    
+    return answer;
+}
