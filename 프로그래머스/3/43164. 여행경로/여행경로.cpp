b4ba4fce49b7121bace6741db7ae2fa17503f7a3
t_woong <113299829+taewoong21@users.noreply.github.com>
2025-12-29 11:11:27 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<string> answer;
+bool visited[10002]; 
+bool isFinished = false; 
+
+void dfs(string cur, vector<string>& routes, vector<vector<string>>& tickets) 
+{
+    
+    if (isFinished) return;
+                                 
+    if (routes.size() == tickets.size() + 1) 
+    {
+        answer = routes; 
+        isFinished = true; 
+        return;
+    }
+
+    // 3. 현재 공항(cur)에서 갈 수 있는 다음 공항 탐색
+    for (int i = 0; i < tickets.size(); ++i) 
+    {
+        if (!visited[i] && tickets[i][0] == cur) 
+        {
+            visited[i] = true;
+            routes.push_back(tickets[i][1]);
+
+            dfs(tickets[i][1], routes, tickets);
+
+            if (isFinished) return;
+
+            routes.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) 
+{
+    answer.clear();
+    isFinished = false;
+   
+    fill(visited, visited + tickets.size(), false);
+
+    sort(tickets.begin(), tickets.end());
+
+    vector<string> routes;
+    routes.push_back("ICN"); 
+    
+    dfs("ICN", routes, tickets);
+    
+    return answer;
+}
